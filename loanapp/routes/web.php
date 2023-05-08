308e70e9900d7634b61cf049fd932565476b386e
Hunaina Ehsan <neelathotaCuSO4@gmail.com>
2023-05-08 11:37:18 +0500

    in web.php

--- loanapp/routes/web.php
+++ loanapp/routes/web.php
@@ -16,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
-    return view('welcome');
-});
+
+Route::get('/register', [UserController::class, 'create']);
